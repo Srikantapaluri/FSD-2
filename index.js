f52f0f69b88022911055221c2de1ce56db7928ec
Srikantapaluri <bhanudoyasrikantapaluri@gmail.com>
2025-07-26 11:36:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+<html>
+    <head>
+        <title>Paluri Bhanudoya Sri Kanta</title>
+    </head>
+    <body>
+        <h1>Name : Paluri Bhanudoya Sri Kanta</h1>
+        <h2>Reg : 23B91A05L0</h2>
+        <h1>Experiment Links:</h1>
+        <ul>
+            <li>Exp 1.A : <a href = "https://fsd-1a-n5ob.onrender.com" target="_blank" >Basic Setup</a></li>
+            <li>Exp 1.B : <a href = "https://fsd-1b-42ol.onrender.com" target="_blank">HTTP Data Transfer</a></li>
+            <li>Exp 1.C : <a href = "https://fsd-1c-o3qq.onrender.com" target="_blank">Custom MathUtil Module</a></li>
+            <li>Exp 1.D : <a href = "https://fsd-1d-30h4.onrender.com" target="_blank">URL Parser</a></li>
+        </ul>
+        
+    </body> 
+</html>
